e1d7c4a1477ce20bac1d8a7116dc0f8e54e76f78
Guillermo <guillermo@guillermo-N56VM.(none)>
2012-11-05 22:09:43 +0800

    HW3 done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    
+    #puts "inserting..." + movie.to_s
+
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +18,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  assert page.body.index(e1) < page.body.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +29,31 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+
+  rating_list.split(",").each do |rating|
+
+    field_id = "ratings[" + rating.strip + "]"
+
+    if not uncheck.nil?
+      #puts "unchecking..." + field_id
+      uncheck(field_id)
+    else
+      #puts "checking..." + field_id
+      check(field_id)
+    end
+
+  end
+
+end
+
+Then /I should see (none|all) of the movies/ do |option|
+
+  if option == 'none'
+    #puts 'oh---none#' + all("table#movies tr").count.to_s
+    assert_equal 0,all("table#movies tr").count-1
+  else
+    #puts 'oh---all#' + all("table#movies tr").count.to_s
+    assert_equal Movie.count,all("table#movies tr").count-1
+  end
+
 end
